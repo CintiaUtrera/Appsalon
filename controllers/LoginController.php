1182c72e3b6add42e9baaef9cd18d59dc4233609
Cintia <cintiaa_utrera@outlook.es>
2023-10-03 20:42:17 -0300

    creando el routing

---
+++ controllers/LoginController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Controllers;
+
+use MVC\Router;
+
+class LoginController{
+    public static function login(Router $router){
+
+    }
+
+    public static function logout(Router $router){
+        
+    }
+
+    public static function olvide(Router $router){
+        
+    }
+
+    public static function recuperar(Router $router){
+        
+    }
+
+    public static function crear(Router $router){
+        
+    }
+}

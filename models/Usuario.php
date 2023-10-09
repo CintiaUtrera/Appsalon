c433a0b5212f0db411e6f1207dd844cac4dfc6af
Cintia <cintiaa_utrera@outlook.es>
2023-10-09 11:53:24 -0300

    creando modelo de usuario

---
+++ models/Usuario.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Model;
+
+class Usuario extends ActiveRecord{
+    // base de datos
+    protected static $tabla = 'usuarios';
+    protected static $columnasDB = ['id', 'nombre', 'apellido', 'email', 'password', 'telefono', 'admin','confirmado', 'token'];
+
+    public $id;
+    public $nombre;
+    public $apellido;
+    public $email;
+    public $password;
+    public $telefono;
+    public $admin;
+    public $confirmado;
+    public $token;
+}

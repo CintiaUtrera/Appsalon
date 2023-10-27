9dfcffca885a318a4ad444ef3159a4cece943e1f
Cintia <cintiaa_utrera@outlook.es>
2023-10-27 12:06:11 -0300

    creando routing para cita

--- public/index.php
+++ public/index.php
@@ -2,8 +2,10 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
-use Controllers\LoginController;
 use MVC\Router;
+
+use Controllers\CitaController;
+use Controllers\LoginController;
 
 $router = new Router();
 
@@ -27,6 +29,10 @@ $router->post('/crear-cuenta', [LoginController::class, 'crear']);
 // confirmar cuenta
 $router->get('/confirmar-cuenta', [LoginController::class, 'confirmar']);
 $router->get('/mensaje', [LoginController::class, 'mensaje']);
+
+// AREA PRIVADA
+$router->get('/cita', [CitaController::class, 'index']);
+
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();

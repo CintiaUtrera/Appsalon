ead3ebe098b99ab8183c210bd789b8b2353a7ce5
Cintia <cintiaa_utrera@outlook.es>
2023-10-30 12:36:33 -0300

    creando botones paginador

--- views/cita/index.php
+++ views/cita/index.php
@@ -39,4 +39,9 @@
         <h2>Resumen</h2>
         <p text-center>Verifica que la información sea correcta</p>
     </div>
+
+    <div class="paginacion">
+        <button id="anterior" class="boton">&laquo; Anterior</button>
+        <button id="anterior" class="boton">Siguiente &raquo;</button>
+    </div>
 </div>

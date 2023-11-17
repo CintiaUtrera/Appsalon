21346ef855f06c66d556d99510cc6829aff89028
Cintia <cintiaa_utrera@outlook.es>
2023-11-17 12:01:26 -0300

    calcular total a pagar

--- views/admin/index.php
+++ views/admin/index.php
@@ -17,10 +17,10 @@
     <ul class="citas">
         <?php 
             $idCita = 0;
-            foreach($citas as $cita){
+            foreach($citas as $key => $cita){
                 if($idCita !== $cita->id){
 
-                    $idCita = $cita->id;
+                $total = 0;
         ?>
         <li>
             <p>ID: <span><?php echo $cita->id; ?></span></p>
@@ -31,9 +31,19 @@
 
             <h3>Servicios</h3>
             
-            <?php } //fin del if ?>
+            <?php $idCita = $cita->id;
+            } //fin del if
+                $total+= $cita->precio;
+            ?>
             <p class="servicio"><?php echo $cita->servicio . " " . $cita->precio; ?></p>
-        
-        <?php } //fin del foreach ?>
+                
+            <?php 
+                $actual = $cita->id;
+                $proximo = $citas[$key + 1]->id ?? 0;
+
+                if(esUltimo($actual, $proximo)){ ?>
+                    <p class="total">Total: <span>$ <?php echo $total; ?></span></p>
+            <?php } 
+         } //fin del foreach ?>
     </ul>
 </div>

cd836f9f31878170d3a99a00fafb1c1cdd9d64e9
Cintia <cintiaa_utrera@outlook.es>
2023-11-21 20:34:21 -0300

    css a los servicios

--- views/servicios/index.php
+++ views/servicios/index.php
@@ -9,6 +9,15 @@
         <li>
             <p>Nombre: <span><?php echo $servicio->nombre; ?></span></p>
             <p>Precio: <span>$<?php echo $servicio->precio; ?></span></p>
+
+            <div class="acciones">
+                <a class="boton" href="/servicios/actualizar?id=<?php echo $servicio->id; ?>">Actualizar</a>
+                <form action="/servicios/eliminar" method="POST">
+                    <input type="hidden" name="id" value="<?php echo $servicio->id; ?>" />
+                    <input type="submit" value="Borrar" class="boton-eliminar" />
+                </form>
+            </div>
+        
         </li>
     <?php } ?>
 </ul> 

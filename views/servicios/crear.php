338919222252dd7bdb636a329c51c93c7f6cd7dc
Cintia <cintiaa_utrera@outlook.es>
2023-11-21 12:20:46 -0300

    creando formulario pñara servicios

--- views/servicios/crear.php
+++ views/servicios/crear.php
@@ -2,3 +2,8 @@
 <p class="drescripcion-pagina">LLena todos los campos para añadir un nuevo Servicio</p>
 
 <?php include_once __DIR__ . '/../templates/barra.php'; ?>
+
+<form action="/servicios/crear" method="POST" class="formulario">
+    <?php include_once __DIR__ . '/formulario.php'; ?>
+    <input type="submit" class="boton" value="Guardar Servicio">
+</form>

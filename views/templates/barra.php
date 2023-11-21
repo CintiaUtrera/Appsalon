39ca298133fd2597840cff126a56f76489ed3748
Cintia <cintiaa_utrera@outlook.es>
2023-11-21 10:51:00 -0300

    menu para el administrador

--- views/templates/barra.php
+++ views/templates/barra.php
@@ -5,3 +5,13 @@
 
     <a class="boton btn" href="/logout">Cerrar Sesión</a>
 </div>
+
+<?php if(isset($_SESSION['admin'])) { ?>
+    <div class="barra-servicios">
+        <a href="/admin" class="boton">Ver Citas</a>
+        <a href="/servicios" class="boton">Ver Servicios</a>
+        <a href="/servicios/crear" class="boton">Nuevo Servicio</a>
+    </div>
+
+
+<?php } ?>

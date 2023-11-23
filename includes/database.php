f48b6661ed52e6b20c8ed6d6027b78293f954bb1
Cintia <cintiaa_utrera@outlook.es>
2023-11-23 09:38:43 -0300

    creando variables de entorno

--- includes/database.php
+++ includes/database.php
@@ -1,7 +1,8 @@
 <?php
 
-$db = mysqli_connect('localhost', 'root', 'root', 'appsalon');
+$db = mysqli_connect($_ENV['DB_HOST'], $_ENV['DB_USER'], $_ENV['DB_PASS'], $_ENV['DB_NAME']);
 
+$db->set_charset('uft8');
 
 if (!$db) {
     echo "Error: No se pudo conectar a MySQL.";
